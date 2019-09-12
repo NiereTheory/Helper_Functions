c70b1d1ac1406a7ad94856100ded37f629c984bf
Niere Theory <bniere@gmail.com>
2019-09-12 08:49:09 -0500

    dim_date proc first pass completed

---
+++ dim_date/generate_dim_date.py
@@ -0,0 +1,64 @@
+from datetime import date, timedelta, datetime
+from calendar import monthrange
+import pandas as pd
+
+date_int_format = '%Y%m%d'
+date_str_format = '%Y-%m-%d'
+
+def fns_date(date_id: int):
+    """Convert an integer formatted date to a standard US date format"""
+    return datetime.strptime(str(date_id), date_int_format).strftime(date_str_format)
+
+def fns_date_id(dt: datetime):
+    """Convert a python date to an integer date_id"""
+    return int(dt.strftime(date_int_format))
+
+def gen_dim_date(start_date=date(2019, 1, 1), end_date=date(2019, 12, 31)):
+    """Generate a potentail dim_date table in a data warehouse"""
+    delta = end_date - start_date
+    dim_date = []
+
+    for i in range(delta.days + 1):
+        current_date_dict = {}
+        python_date = start_date + timedelta(days=i)
+        # current_date_dict['PYTHON_DATE'] = python_date
+        
+        current_date_dict['DATE_ID'] = fns_date_id(python_date)
+        
+        current_date_dict['FULL_DATE'] = fns_date(current_date_dict['DATE_ID'])
+        
+        current_date_dict['MONTH_VALUE'] = python_date.month
+
+        current_date_dict['FIRST_DAY_OF_MONTH'] = python_date.replace(day=1)
+        
+        monthrange(python_date.year, python_date.month)
+        current_date_dict['LAST_DAY_OF_MONTH'] = python_date. \
+            replace(day=monthrange(python_date.year, python_date.month)[1])
+
+        current_date_dict['QUARTER_VALUE'] = (python_date.month - 1) // 3 + 1
+        
+        qtr_val = current_date_dict['QUARTER_VALUE']
+        current_date_dict['FIRST_DAY_OF_QUARTER'] = python_date. \
+            replace(month=qtr_val * 3 - 2, day=1)
+        
+        last_quarter_month_start = date(python_date.year, qtr_val * 3, 1)
+        current_date_dict['LAST_DAY_OF_QUARTER'] = last_quarter_month_start. \
+                replace(day=monthrange(last_quarter_month_start.year, last_quarter_month_start.month)[1])
+
+        current_date_dict['FIRST_DAY_OF_YEAR'] = date(python_date.year, 1, 1)
+        
+        current_date_dict['LAST_DAY_OF_YEAR'] = date(python_date.year, 12, 31)
+
+        retain_format = tuple(['DATE_ID', 'FULL_DATE', 'MONTH_VALUE', 'QUARTER_VALUE'])
+        final_date_dict = {k: v if k in retain_format else fns_date_id(v) for k, v in current_date_dict.items()}
+        dim_date.append(final_date_dict)
+
+    return dim_date
+
+if __name__ == '__main__':
+    try:
+        df = pd.DataFrame(gen_dim_date())
+        df.to_csv('dim_date.csv', index=False)
+        print('Process completed!')
+    except Exception as e:
+        print(f'Error occurred: {e}')
---
+++ dim_date/test_dim_date.py
@@ -0,0 +1,41 @@
+import unittest
+from generate_dim_date import fns_date, fns_date_id, gen_dim_date
+from datetime import date
+
+class TestDimDate(unittest.TestCase):
+
+    def setUp(self):
+        self.dim_date = gen_dim_date()
+
+    def test_fns_date(self):
+        self.assertEqual(fns_date(20190101), '2019-01-01')
+
+    def test_fns_date_id(self):
+        self.assertEqual(fns_date_id(date(2019, 1, 1)), 20190101)
+
+    def test_dim_date(self):
+        self.assertEqual(len(self.dim_date), 365)
+        
+        first_day = self.dim_date[0]
+        self.assertEqual(first_day['DATE_ID'], 20190101)
+        self.assertEqual(first_day['FULL_DATE'], '2019-01-01')
+        self.assertEqual(first_day['MONTH_VALUE'], 1)
+        self.assertEqual(first_day['FIRST_DAY_OF_MONTH'], 20190101)
+        self.assertEqual(first_day['LAST_DAY_OF_MONTH'], 20190131)
+        self.assertEqual(first_day['QUARTER_VALUE'], 1)
+        self.assertEqual(first_day['FIRST_DAY_OF_QUARTER'], 20190101)
+        self.assertEqual(first_day['LAST_DAY_OF_QUARTER'], 20190331)
+        self.assertEqual(first_day['FIRST_DAY_OF_YEAR'], 20190101)
+        self.assertEqual(first_day['LAST_DAY_OF_YEAR'], 20191231)
+
+        random_day = [d for d in self.dim_date if d['DATE_ID'] == 20190720][0]
+        self.assertEqual(random_day['DATE_ID'], 20190720)
+        self.assertEqual(random_day['FULL_DATE'], '2019-07-20')
+        self.assertEqual(random_day['MONTH_VALUE'], 7)
+        self.assertEqual(random_day['FIRST_DAY_OF_MONTH'], 20190701)
+        self.assertEqual(random_day['LAST_DAY_OF_MONTH'], 20190731)
+        self.assertEqual(random_day['QUARTER_VALUE'], 3)
+        self.assertEqual(random_day['FIRST_DAY_OF_QUARTER'], 20190701)
+        self.assertEqual(random_day['LAST_DAY_OF_QUARTER'], 20190930)
+        self.assertEqual(random_day['FIRST_DAY_OF_YEAR'], 20190101)
+        self.assertEqual(random_day['LAST_DAY_OF_YEAR'], 20191231)
