828aff3faf845521f958a495856ab953c79b7631
Niere Theory <bniere@gmail.com>
2019-09-18 14:01:02 -0500

    ci/cd first step

--- DWDateTime/dim_date/test_dim_date.py
+++ DWDateTime/dim_date/test_dim_date.py
@@ -12,49 +12,48 @@ class TestDimDate(unittest.TestCase):
         self.dim_date = DimDate(date_str_format=r'%Y-%m-%d')
 
     def test_fns_date(self):
-        self.assertEqual(self.dim_date.fns_date(20190101), '2019-01-01')
+        assert self.dim_date.fns_date(20190101) == '2019-01-01'
 
     def test_fns_date_id(self):
-        self.assertEqual(self.dim_date.fns_date_id(date(2019, 1, 1)), 20190101)
+        assert self.dim_date.fns_date_id(date(2019, 1, 1)) == 20190101
 
     def test_dim_date(self):
         df_dim_date = self.dim_date.gen_dim_date()
-        self.assertEqual(len(df_dim_date), 365)
+        assert len(df_dim_date) == 365
 
         first_day_to_test = 20190101
         first_day = pd.DataFrame(
             df_dim_date[df_dim_date.DATE_ID == first_day_to_test])
-        self.assertEqual(first_day['DATE_ID'].values[0], 20190101)
-        self.assertEqual(first_day['FULL_DATE'].values[0], '2019-01-01')
-        self.assertEqual(first_day['DAY_VALUE'].values[0], 1)
-        self.assertEqual(first_day['WEEKDAY_NAME'].values[0], 'Tuesday')
-        self.assertEqual(first_day['IS_WEEKDAY'].values[0], 1)
-        self.assertEqual(first_day['MONTH_VALUE'].values[0], 1)
-        self.assertEqual(first_day['MONTH_NAME'].values[0], 'January')
-        self.assertEqual(first_day['FIRST_DAY_OF_MONTH'].values[0], 20190101)
-        self.assertEqual(first_day['LAST_DAY_OF_MONTH'].values[0], 20190131)
-        self.assertEqual(first_day['QUARTER_VALUE'].values[0], 1)
-        self.assertEqual(first_day['FIRST_DAY_OF_QUARTER'].values[0], 20190101)
-        self.assertEqual(first_day['LAST_DAY_OF_QUARTER'].values[0], 20190331)
-        self.assertEqual(first_day['FIRST_DAY_OF_YEAR'].values[0], 20190101)
-        self.assertEqual(first_day['LAST_DAY_OF_YEAR'].values[0], 20191231)
+        assert first_day['DATE_ID'].values[0] == 20190101
+        assert first_day['FULL_DATE'].values[0] == '2019-01-01'
+        assert first_day['DAY_VALUE'].values[0] == 1
+        assert first_day['WEEKDAY_NAME'].values[0] == 'Tuesday'
+        assert first_day['IS_WEEKDAY'].values[0] == 1
+        assert first_day['MONTH_VALUE'].values[0] == 1
+        assert first_day['MONTH_NAME'].values[0] == 'January'
+        assert first_day['FIRST_DAY_OF_MONTH'].values[0] == 20190101
+        assert first_day['LAST_DAY_OF_MONTH'].values[0] == 20190131
+        assert first_day['QUARTER_VALUE'].values[0] == 1
+        assert first_day['FIRST_DAY_OF_QUARTER'].values[0] == 20190101
+        assert first_day['LAST_DAY_OF_QUARTER'].values[0] == 20190331
+        assert first_day['FIRST_DAY_OF_YEAR'].values[0] == 20190101
+        assert first_day['LAST_DAY_OF_YEAR'].values[0] == 20191231
 
         random_day = df_dim_date[df_dim_date.DATE_ID == 20190720]
-        self.assertEqual(random_day['DATE_ID'].values[0], 20190720)
-        self.assertEqual(random_day['FULL_DATE'].values[0], '2019-07-20')
-        self.assertEqual(random_day['DAY_VALUE'].values[0], 20)
-        self.assertEqual(random_day['WEEKDAY_NAME'].values[0], 'Saturday')
-        self.assertEqual(random_day['IS_WEEKDAY'].values[0], 0)
-        self.assertEqual(random_day['MONTH_VALUE'].values[0], 7)
-        self.assertEqual(random_day['MONTH_NAME'].values[0], 'July')
-        self.assertEqual(random_day['FIRST_DAY_OF_MONTH'].values[0], 20190701)
-        self.assertEqual(random_day['LAST_DAY_OF_MONTH'].values[0], 20190731)
-        self.assertEqual(random_day['QUARTER_VALUE'].values[0], 3)
-        self.assertEqual(
-            random_day['FIRST_DAY_OF_QUARTER'].values[0], 20190701)
-        self.assertEqual(random_day['LAST_DAY_OF_QUARTER'].values[0], 20190930)
-        self.assertEqual(random_day['FIRST_DAY_OF_YEAR'].values[0], 20190101)
-        self.assertEqual(random_day['LAST_DAY_OF_YEAR'].values[0], 20191231)
+        assert random_day['DATE_ID'].values[0] == 20190720
+        assert random_day['FULL_DATE'].values[0] == '2019-07-20'
+        assert random_day['DAY_VALUE'].values[0] == 20
+        assert random_day['WEEKDAY_NAME'].values[0] == 'Saturday'
+        assert random_day['IS_WEEKDAY'].values[0] == 0
+        assert random_day['MONTH_VALUE'].values[0] == 7
+        assert random_day['MONTH_NAME'].values[0] == 'July'
+        assert random_day['FIRST_DAY_OF_MONTH'].values[0] == 20190701
+        assert random_day['LAST_DAY_OF_MONTH'].values[0] == 20190731
+        assert random_day['QUARTER_VALUE'].values[0] == 3
+        assert random_day['FIRST_DAY_OF_QUARTER'].values[0] == 20190701
+        assert random_day['LAST_DAY_OF_QUARTER'].values[0] == 20190930
+        assert random_day['FIRST_DAY_OF_YEAR'].values[0] == 20190101
+        assert random_day['LAST_DAY_OF_YEAR'].values[0] == 20191231
 
 
 if __name__ == '__main__':
--- DWDateTime/dim_time/test_dim_time.py
+++ DWDateTime/dim_time/test_dim_time.py
@@ -11,38 +11,37 @@ class TestDimTime(unittest.TestCase):
         self.dim_time = DimTime()
 
     def test_right_justify(self):
-        self.assertEqual(self.dim_time.right_justify(3), '03')
+        assert self.dim_time.right_justify(3) == '03'
 
     def test_dim_time(self):
         df_dim_time: pd.DataFrame = self.dim_time.gen_dim_time()
-        self.assertEqual(len(df_dim_time), 86400)
+        assert len(df_dim_time) == 86400
 
         first_time_to_test = '000430'
         first_time = pd.DataFrame(
             df_dim_time[df_dim_time.TIME_ID == first_time_to_test])
-        self.assertEqual(first_time['TIME_ID'].values[0], '000430')
-        self.assertEqual(first_time['MILITARY_HOUR'].values[0], '00')
-        self.assertEqual(first_time['HOUR'].values[0], '00')
-        self.assertEqual(first_time['MINUTE'].values[0], '04')
-        self.assertEqual(first_time['SECOND'].values[0], '30')
-        self.assertEqual(first_time['MILITARY_COMBINED'].values[0], '00:04:30')
-        self.assertEqual(first_time['COMBINED'].values[0], '00:04:30')
-        self.assertEqual(first_time['AM_PM'].values[0], 'AM')
-        self.assertEqual(first_time['BUSINESS_HOUR'].values[0], 0)
+        assert first_time['TIME_ID'].values[0] == '000430'
+        first_time['MILITARY_HOUR'].values[0] == '00'
+        first_time['HOUR'].values[0] == '00'
+        first_time['MINUTE'].values[0] == '04'
+        first_time['SECOND'].values[0] == '30'
+        first_time['MILITARY_COMBINED'].values[0], '00: ==4:30'
+        first_time['COMBINED'].values[0], '00: ==4:30'
+        first_time['AM_PM'].values[0] == 'AM'
+        first_time['BUSINESS_HOUR'].values[0] == 0
 
         random_time_to_test = '153000'
         random_time = pd.DataFrame(
             df_dim_time[df_dim_time.TIME_ID == random_time_to_test])
-        self.assertEqual(random_time['TIME_ID'].values[0], '153000')
-        self.assertEqual(random_time['MILITARY_HOUR'].values[0], '15')
-        self.assertEqual(random_time['HOUR'].values[0], '03')
-        self.assertEqual(random_time['MINUTE'].values[0], '30')
-        self.assertEqual(random_time['SECOND'].values[0], '00')
-        self.assertEqual(
-            random_time['MILITARY_COMBINED'].values[0], '15:30:00')
-        self.assertEqual(random_time['COMBINED'].values[0], '03:30:00')
-        self.assertEqual(random_time['AM_PM'].values[0], 'PM')
-        self.assertEqual(random_time['BUSINESS_HOUR'].values[0], 1)
+        assert random_time['TIME_ID'].values[0] == '153000'
+        assert random_time['MILITARY_HOUR'].values[0] == '15'
+        assert random_time['HOUR'].values[0] == '03'
+        assert random_time['MINUTE'].values[0] == '30'
+        assert random_time['SECOND'].values[0] == '00'
+        assert random_time['MILITARY_COMBINED'].values[0] == '15:30:00'
+        assert random_time['COMBINED'].values[0] == '03:30:00'
+        assert random_time['AM_PM'].values[0] == 'PM'
+        assert random_time['BUSINESS_HOUR'].values[0] == 1
 
 
 if __name__ == '__main__':
